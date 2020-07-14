ba0ad09ecbfd99b04e5e177d95e6d8ee62632f25
thehippiegangster <59704591+thehippiegangster@users.noreply.github.com>
2020-07-14 11:21:24 -0700

    updated wsgi

--- payroll/wsgi.py
+++ payroll/wsgi.py
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'payroll.settings')
 
 application = get_wsgi_application()
-
-from payroll.wsgi import payrollsite
-application = payrollsite(application)
